42cfff1fe0bc2d0a1aef8d35c9024fde9e406899
zuxbrt <33069559+zuxbrt@users.noreply.github.com>
2021-01-28 07:29:28 +0000

    Update AdminViewsController.php
    sorting fix

--- app/Http/Controllers/AdminViewsController.php
+++ app/Http/Controllers/AdminViewsController.php
@@ -101,22 +101,27 @@ class AdminViewsController extends Controller
 
             $filtered = collect();
             $users = User::where('type_of_user', '!=', '2')->get();
+            $msg_count = Message::all()->count();
 
-            if(Message::all()->count() > 0){
+            if($msg_count > 0){
                 foreach($categories as $category){
                     foreach($users as $user){
                         $message = Message::where('category', $category)->where('user_channel', $user->id)->orderBy('created_at', 'desc')->get()->first();
-                        $filtered->push($message);
+                        if($message){
+                            $filtered->push($message);
+                        }
                     }
                 }
     
-                $filtered = $filtered->sort(function ($a, $b){
-                    if ($a->total === $b->total) {
-                        return strtotime($a->created_at) < strtotime($b->created_at);
-                    }
-                    
-                    return $a->total < $b->total;
-                });
+                if($msg_count > 1){
+                    $filtered = $filtered->sort(function ($a, $b){
+                        if ($a->total === $b->total) {
+                            return strtotime($a->created_at) < strtotime($b->created_at);
+                        }
+                        
+                        return $a->total < $b->total;
+                    });
+                }
             }
 
         }
